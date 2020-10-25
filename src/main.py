fcd8a10191d0f1840925daa5776aa0bac7d147d2
nagisc007 <nagisc007@yahoo.co.jp>
2020-10-25 16:56:21 +0900

    Add struct ep3

--- src/main.py
+++ src/main.py
@@ -161,8 +161,15 @@ def ch_sadness_valley(w: World):
             w.plot_setup("その事件の容疑者が被害者の娘$maryだった"),
             w.plot_setup("$maryの家の使用人は彼女の無実を訴える"),
             w.plot_turnpoint("話を聞いて$sherlockは$maryの無実を確信する"),
-            w.plot_turnpoint(""),
-            w.plot_resolve(""),
+            w.plot_develop("現地に赴き、調査する$sherlock"),
+            w.plot_develop("亡くなった男性の家は地元でも有数の金持ちだった"),
+            w.plot_develop("一人娘の$maryを愛していたはずの父親と何故口論していたのか、$maryは話したがらない"),
+            w.plot_develop("警察に連れて行かれてしまう$mary"),
+            w.plot_turnpoint("$maryの部屋で母親の死体が発見される"),
+            w.plot_resolve("$maryをはめたのは、使用人の父親だった"),
+            w.plot_resolve("婦人との間に作った自分の息子を正当な跡取りとするために、旦那を殺害した"),
+            w.plot_resolve("しかし$maryが拾われた子どもだと知り、かつて愛した女を殺してしまった"),
+            w.plot_resolve("身寄りを失った$maryを$sherlockは引き取った"),
             )
 
 def ch_red_armor(w: World):
