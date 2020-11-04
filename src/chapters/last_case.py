11c8a3929458f639e2e6e39e915625a1da9a1b9e
nagisc007 <nagisc007@yahoo.co.jp>
2020-11-04 17:12:12 +0900

    [WIP] Add plot:ep6

--- src/chapters/last_case.py
+++ src/chapters/last_case.py
@@ -27,6 +27,22 @@ def main(w: World):
             w.plot_resolve("全てが仕組まれていたことだと分かりながらここにやってきた$sherlock"),
             w.plot_resolve("しかし屋敷に続く橋が爆破され、警官隊は来られなかった"),
             w.plot_resolve("$sherlockが$morianoと闇の穴に落ちたと目撃証言が入った"),
+            "不穏な空気",
+            w.plot_note("あれから一ヶ月。$maryたちのいる暮らしにも慣れた"),
+            w.plot_note("$limeが意外と料理を覚えるのが早くて、二人でわいわいやっている"),
+            w.plot_note("$sherlockは新聞を見ながらうなっている"),
+            w.plot_note("最近さまざまな、不可解な犯罪が増えている"),
+            w.plot_note("そこに一通の書簡が届く"),
+            # TODO
+            "$sherlockが穴に落ちた",
+            w.plot_note("彼が事前に出した手紙が届く"),
+            w.plot_note("そこにはこの手紙が届いたということは自分が亡くなった可能性があると書かれている"),
+            w.plot_note("$sherlockは自分を殺したのは$morianoで、彼が亡くなったときには新聞のある部分に知らせが出るとある"),
+            w.plot_note("$wilsonがその知らせを見つける"),
+            w.plot_note("$sherlockからの手紙には$morianoが倒れても彼の意志を貫くと"),
+            w.plot_note("それに気になるところがあり、自分が知っている人間にその情報を探らせているともあった"),
+            "まだ生きているはず",
+            w.plot_note("$sherlockが本当になくなったとは信じたくない$maryは、探すと言い出して街に出ていく"),
             )
 
 
