ef3b18481a0aea46b72d3e81d0a3e71290ab04b2
nagisc007 <nagisc007@yahoo.co.jp>
2020-11-17 14:18:08 +0900

    Add plot:ep5

--- src/chapters/devil_dog.py
+++ src/chapters/devil_dog.py
@@ -82,10 +82,33 @@ def exist_darkdog(w: World):
             )
 
 
+def basement_room(w: World):
+    return w.episode("地下室",
+            w.plot_note("天候が回復し、船舶操作に慣れた男が警察を呼んでくると何人か連れて島を離れる"),
+            w.plot_note("その間に$sherlockは$cherryにもう一度城を探させてほしいとお願いする"),
+            w.plot_note("彼女はあまり気が進まないようだが許可を出す"),
+            w.plot_note("$sherlockは壁に血の跡を見つけ、そこから地下への入り口を発見する"),
+            w.plot_note("地下にはかつての拷問部屋が存在していた"),
+            w.plot_note("そこで新しい血の跡を発見する"),
+            w.plot_note("振り返ると$cherryがいた"),
+            w.plot_note("彼女は近代まで使われていたと語る"),
+            w.plot_note("実際事故死した彼女の夫もそれを使い、使用人を何人も病院送りにしているらしい"),
+            w.plot_note("そこに犬用のトレイを見つけた"),
+            w.plot_note("$sherlockはここで犬を買っていることを示唆するが、$cherryは強く否定する"),
+            w.plot_note("だがそこに大きな黒い犬が出現する"),
+            w.plot_note("殺そうとしたところに、$cherryは体を張って立ちふさがった"),
+            )
+
+
 def real_murder(w: World):
     return w.episode("犯人",
-            "$sherlockに犯人と指摘された女主人が逃げ出した",
-            # TODO
+            w.plot_note("$sherlockは全ての犯行が彼女によるものだと断言する"),
+            w.plot_note("全ての犯行が獣にやられるように見せかけられていたが、この拷問室にある道具には獣の歯型をした拷問具があった"),
+            w.plot_note("それに新しい血がついているので、これが使われたと"),
+            w.plot_note("この場所を知っていた人間は$cherry以外に誰かいるのか尋ねると、$mockもだと語った"),
+            w.plot_note("彼女は全ての計画は彼がねって実行したものだと言う"),
+            w.plot_note("しかし$sherlockは彼がここに赴任して三年目だということを知っていた"),
+            w.plot_note("犯人だと論破された$cherryは部屋を真っ暗にして逃げ出す"),
             )
 
 
@@ -110,7 +133,6 @@ def sad_end(w: World):
 
 def rebirth_ritual(w: World):
     return w.episode("蘇りの技法",
-            "蘇りの秘法により起こった悲劇だった",
             w.plot_note("女主人$cherryの死去により事件は全てに幕を下ろした"),
             w.plot_note("死後に見つかった手記により今までの大半の事件の陰に彼女の存在があったことが判明する"),
             w.plot_note("$sherlockは死後に秘密の地下室を発見し、そこで儀式の跡を見た"),
@@ -144,6 +166,7 @@ def main(w: World):
             first_murder(w),
             second_murder(w),
             exist_darkdog(w),
+            basement_room(w),
             real_murder(w),
             sad_end(w),
             rebirth_ritual(w),
