cc389a9daf99abce1c88dc08f0ba5007a5c4c9a1
nagisc007 <nagisc007@yahoo.co.jp>
2020-11-19 16:09:44 +0900

    [WIP] Add plot:ep3

--- src/chapters/red_armor_club.py
+++ src/chapters/red_armor_club.py
@@ -10,7 +10,44 @@ from storybuilder.builder.world import World
 from config import TITLES
 
 
-## chapter
+# Episodes
+def missing_persons(w: World):
+    return w.episode("失踪者たち",
+            "$maryは困っていたという謎の鎧騎士を連れてくる",
+            )
+
+
+def strange_work(w: World):
+    return w.episode("不思議な仕事",
+            "$sherlockは$limeにその仕事をすぐ辞めるように進言したが、理由は話さなかった",
+            )
+
+
+def bank_robbery(w: World):
+    return w.episode("銀行強盗",
+            "$limeが仕事を首になったらしいと$maryから伝え聞いた",
+            )
+
+
+def truth_of_club(w: World):
+    return w.episode("クラブの真相",
+            "$limeが助けてほしいとやってくる",
+            )
+
+
+def what_was_stolen(w: World):
+    return w.episode("盗まれたもの",
+            "$limeは自分が王室の人間であることを告白した",
+            )
+
+
+def new_living(w: World):
+    return w.episode("新しい居候",
+            "$limeが$sherlockたちの新しい居候になった",
+            )
+
+
+# Chapter
 def main(w: World):
     return w.chapter(TITLES[3],
             w.plot_setup("失踪者のリストの中に第二王女も含まれている"),
@@ -24,45 +61,12 @@ def main(w: World):
             w.plot_develop("犯人たちは既に逃げたあとで同僚の兵士は殺されて見つかった"),
             w.plot_turnpoint("$limeは事情があって王室に戻れないと告白した"),
             w.plot_resolve("$limeが$sherlockの家に居候することになった"),
-            "奇妙な仕事",
-            w.plot_note("$maryを家に迎えたが、彼女は家事全般が苦手で、あまり役立たない"),
-            w.plot_note("それでも$sherlockを気に入り、秘書まがいのことをやっている"),
-            w.plot_note("その$maryが買い物から帰ってきたら、奇妙な赤鎧の人間を連れてきた"),
-            w.plot_note("何やら事情を抱えているので話を聞いてあげてほしいと"),
-            w.plot_note("ただその鎧騎士は言葉が話せなかった"),
-            "赤鎧クラブ",
-            w.plot_note("鎧騎士は$limeと名乗った（実際は筆談である）"),
-            w.plot_note("彼の言うところではある質屋の家に世話になっているがそこの護衛を行うことで食費代にしている"),
-            w.plot_note("そこは雇いバイトの男$binsがいて、彼が店の切り盛りを、護衛を騎士が行っているらしい"),
-            w.plot_note("その$binsに紹介され、小遣い稼ぎにとあるバイトを紹介してもらったという"),
-            w.plot_note("それは赤い鎧を着た人間にだけ許可された特別なもので「赤鎧クラブ」の仕事だ"),
-            w.plot_note("毎日事務所に行き、三時間、大事な古文書の写本を行うだけで週給７０Gももらえるらしい"),
-            w.plot_note("ただ内緒でそのアルバイトをしていて、その分け前を護衛仲間に渡しているのが心に引っかかるらしい"),
-            w.plot_note("$sherlockは護衛の方は気にしなくても実際に問題は起こっていないだろうと断言する"),
-            w.plot_note("ただその仕事はすぐに辞めた方がいいと進言する"),
-            w.plot_note("そうしようと思いますと答えて、彼は帰っていった"),
-            "仕事を首になる",
-            w.plot_note("数日後、再びその赤鎧の騎士が訪れた"),
-            w.plot_note("仕事そのものをクビになったというのだ"),
-            w.plot_note("詳細を聞くと、今朝、いきなり警察が入ってきて家探しをしたらしい"),
-            w.plot_note("事情を聞くと働いていた仲間が捕まったという"),
-            w.plot_note("質屋のものを横流ししていたというのでオーナーが激怒して、それに騎士が加担したことになっていたらしい"),
-            w.plot_note("仲間を信頼したのに裏切られた形になった"),
-            w.plot_note("しかし$sherlockはそれは本来の目的ではないと断言し、騎士を連れ立ってその質屋へと向かう"),
-            "本当に盗まれたもの",
-            w.plot_note("$sherlockはそのオーナーである$jakinsに迷惑な顔をされつつも、勝手に中に入り、事務所を調べる"),
-            w.plot_note("金庫から盗まれたものがないことを確認すると、その裏側を調べる"),
-            w.plot_note("抜け穴が掘られていて、その先を行くと、隣接する国営博物館に繋がっていた"),
-            w.plot_note("現在改築工事で閉鎖中だったが、展示品だった宝石が盗まれていた"),
-            w.plot_note("それが狙いだったという$sherlock"),
-            w.plot_note("そこに展示されていたのは国宝の一つであるサファイアだった"),
-            "失踪した王女",
-            w.plot_note("結局犯人たちは殺されて発見された"),
-            w.plot_note("解雇された鎧騎士が家にやってくる"),
-            w.plot_note("彼女は自分が失踪中の第二王女だと告白する"),
-            w.plot_note("ただ事情があって王室には戻れないと言う"),
-            w.plot_note("ここでしばらく置いてもらえないかと頼む$lime"),
-            w.plot_note("$maryは友だちができて喜ぶ"),
+            missing_persons(w),
+            strange_work(w),
+            bank_robbery(w),
+            truth_of_club(w),
+            what_was_stolen(w),
+            new_living(w),
             )
 
 
