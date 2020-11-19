65b1ad8b857b54ea241239d5b65d505979ffd106
nagisc007 <nagisc007@yahoo.co.jp>
2020-11-19 10:59:43 +0900

    Add plot:ep4

--- src/chapters/blue_garnet.py
+++ src/chapters/blue_garnet.py
@@ -74,17 +74,27 @@ def whereabouts(w: World):
 
 def reunion_her(w: World):
     return w.episode("彼女との再会",
-            "$jackは自分がはめられたことを告白した",
-            w.plot_note("$sherlockがきたのは"),
-            # TODO
+            w.plot_note("$sherlockがきたのは自分がかつて通っていた学園だった"),
+            w.plot_note("そこの臨時教師になっていた$jackと出会う"),
+            w.plot_note("$jackは自分の将来の夢が教師だったと告白する"),
+            w.plot_note("生まれたときから孤独で、孤児院で大勢の小さい子のために物心ついたころから盗みを働いていた"),
+            w.plot_note("あの$stoneを集めようとしたのは、その孤児院を救うためだったと言う"),
+            w.plot_note("そのクライアントが、なくなっていたもと研究員だった"),
+            w.plot_note("一体なにの研究をしていたのかは分からないが、どこで稼いだのか、かなり金を持っていた"),
+            w.plot_note("実際に家にいったのは一度だけで、その時には既に彼が殺された状態だった"),
+            w.plot_note("仕組んだのは$stoneを手に入れたい別の人間"),
+            w.plot_note("$sherlockはそれを理解した上で、あの$stoneを渡してはいけないと言う"),
+            w.plot_note("$jackは$sherlockに条件を出す"),
+            w.plot_note("それを飲んでくれたら$stoneを渡すと"),
             )
 
 
 def her_message(w: World):
     return w.episode("彼女からのメッセージ",
-            "$jackは$sherlockに$stoneを託して、しばらく世間から姿を消すと言い残して、去っていった",
-            w.plot_note("$jackは自分が狙われた原因が$stoneにあると、それを見せる"),
-            w.plot_note(""),
+            w.plot_note("$sherlockは泥棒の頼みは聞けないというが、一度助けてもらった恩があるといい、その条件を聞く"),
+            w.plot_note("$jackはしばらく遠方に身を隠しながら、孤児院の支援を続けるから、ここが潰れないようにはからってほしいと"),
+            w.plot_note("$jackは自分が狙われた原因が$stoneにあると、持っていたそれを見せる"),
+            w.plot_note("それは三つ目の$stoneだった"),
             w.plot_note("$jackは$sherlockに$stoneを渡す"),
             w.plot_note("誰かがこれを四つ集めて何かしようと企んでいると告げる"),
             w.plot_note("$sherlockになんとしてもそれを阻止してほしいと頼み、彼女は姿を消す"),
