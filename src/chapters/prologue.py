1147683e9ed86d5b73f3c968f11e3f0c920d6ea4
nagisc007 <nagisc007@yahoo.co.jp>
2020-12-22 17:34:43 +0900

    [WIP]

--- src/chapters/prologue.py
+++ src/chapters/prologue.py
@@ -21,16 +21,35 @@ from scenes import WilsonHouse
 
 def troublesome(w: World):
     return w.episode("厄介事",
+            w.plot_setup("$scienceにより夜でも明るい街"),
+            w.plot_setup("$bossが$heroによって倒され、世界は人間が溢れていた"),
+            w.plot_setup("それでも問題は山積で、$wilsonはそんな問題に頭を悩ませている一人"),
+            w.plot_develop("$wilsonは馴染みのバーに立ち寄る"),
+            w.plot_turnpoint("$wilsonに声をかける$stan"),
+            w.plot_develop("$wilsonは今時分が王室から色々と頼まれて厄介事を解決する部署にいると話す"),
+            "お互いに雰囲気が変わった、と話す",
+            w.plot_develop(""),
+            w.plot_turnpoint("知人の$stanから便利屋$sherlockのことを教わる"),
             )
 
 
 def handyman_sherlock(w: World):
     return w.episode("便利屋$sherlock",
+            w.plot_setup("翌朝、$sherlockの家に向かう$wilson"),
+            w.plot_setup("$sherlockの家は$Baker街にある"),
+            w.plot_develop("$carを準備して、$sherlockの家に向かう"),
+            w.plot_develop("産業革命が起こり、街は急成長している"),
+            w.plot_develop("ただところどころに旧時代の名残がある"),
+            w.plot_turnpoint("$carから降りると子供たち（$ignes）に囲まれる"),
+            w.plot_develop("$wilsonは"),# TODO
+            w.plot_turnpoint("何とか$sherlockの家のドアをノックした"),
             )
 
 
 def note_for_novel(w: World):
     return w.episode("作品のための注意書き",
+            w.plot_setup(""),
+            w.plot_resolve(""),
             )
 
 
