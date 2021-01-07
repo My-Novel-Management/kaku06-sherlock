8fa9187ab69bda358e5112d736d38f8e6c259a97
nagisc007 <nagisc007@yahoo.co.jp>
2021-01-07 11:21:10 +0900

    [WIP] epilogue plot

--- src/chapters/c8_epilogue.py
+++ src/chapters/c8_epilogue.py
@@ -54,52 +54,66 @@ $wilsonはそれを口にして昔暮らしていた世界を懐かしく思う�
 # NOTE: case
 #   ・$wilsonすり替わり事件　→本物の$wilsonに長旅をさせ、その間になりすました
 
+
 def total_the_end(w: World):
     return w.episode("全ての顛末",
             # NOTE
             #   ・全てが終わり
-            w.plot_setup("ひとまず$wilsonの家で生活を送っていた$sherlockたちだが、そこに大家がやってきて家賃が滞納されている事実を伝える"),
+            w.plot_setup("偽$wilsonの自殺により一連の事件の幕引きがなされ、ひとまず$wilsonの家で生活を送っていた$sherlockたち"),
             w.plot_note("偽$wilsonの自殺により、一連の事件は終わりを告げた"),
             w.plot_note("住む場所を失い、とりあえず$wilsonの家を間借りして暮らすことにした$sherlockたち"),
+            w.plot_note("今まで食費などの大半が偽$wilsonが出してくれていたので、仕方なく$maryと$limeはアルバイトを続ける"),
+            w.plot_turnpoint("大家がやってきて家賃がずっと滞納していて、今すぐ払えないと出ていってほしいと話す"),
+            w.plot_develop("何とか打開策を考えようとする$maryたちに対して、どこでも本が読めればいいとやる気のない$sherlock"),
+            w.plot_note("$sherlockはそんな金あるはずがないと答える"),
+            w.plot_note("$maryたちは何とか金策をして待ってもらう方法を考える"),
+            w.plot_note("$limeは王室からお金を借りようかとすら考えるが、そこに頼るのはできないと思い直す"),
+            w.plot_note("$maryは遺産を寄付したことを後悔する"),
             w.plot_turnpoint("本物の$wilsonが帰ってくる"),
-            w.plot_develop(""),
-            w.plot_turnpoint(""),
-            w.plot_resolve(""),
-            # TODO
-            w.plot_setup("偽$wilsonが自殺して、全ての幕が降ろされた"),
-            w.plot_setup("$maryは精神的なショック以外に大きな怪我はなく、しばらく入院したのちに退院した"),
-            w.plot_turnpoint("退院した$maryが$limeとともに帰ってくる"),
-            w.plot_develop("$sherlockは大家の$lisaに詰められていた"),
-            w.plot_develop("家賃の支払いが待てないというのだ"),
-            w.plot_develop("仕方なく荷物を手に、外に出る$sherlock"),
-            w.plot_develop("$sherlockは行く宛があると言う"),
-            w.plot_develop("やってきたのは$wilsonの家だった"),
-            w.plot_develop("以前に見つけておいたという合鍵の場所を調べて鍵を手に入れ、中に入る"),
-            w.plot_develop("しばらく$wilsonの家で厄介になることにした$sherlockたち"),
-            w.plot_turnpoint("翌日、そこに知らない男が入ってくる。本物の$wilsonだった"),
-            #   ・本物の$wilson
-            w.plot_develop("$wilsonは自分がいない間に何があったのかを$sherlockから聞く"),
-            w.plot_develop("また何故ここにいるのか、その事情も知る"),
-            w.plot_develop("$wilsonは度々出かけるから、留守の間、家を預かってもらえるのは助かるといいつつ"),
-            w.plot_develop("それでも四人分をまかなえるほどの蓄えはないから、それぞれに仕事をしてもらう必要があると"),
-            w.plot_develop("$maryたちは仕事の宛があった"),
-            w.plot_develop("$sherlockは働く気がない"),
-            w.plot_turnpoint("そこで$wilsonが$sherlockの活躍を小説にして新聞社に売りつけることになった"),
-            )
+            w.plot_develop("$sherlockたちは$wilsonに一連の事件と自分たちがここで暮らしている事情を語る"),
+            w.plot_note("本物の$wilsonなのか疑う$sherlock"),
+            w.plot_note("$wilsonは自分が本物である証拠として、彼しか知らない家の備品の秘密を話す"),
+            w.plot_note("本物の$wilsonだということを認めた上で「残念ながら君はここから出て行かなくてはならない」と告げる$sherlock"),
+            w.plot_note("家賃滞納の事情を語り、理解してもらう"),
+            w.plot_note("$wilsonによると、どうやら家賃分のお金が全て生活費として引き出されていたことが判明した"),
+            w.plot_note("つまり偽$wilsonが支援してくれていた生活費の原資は、本物の$wilsonの家賃だった"),
+            w.plot_note("$wilsonは$sherlockたちが家を失うのは困るだろうと対策を考える"),
+            w.plot_turnpoint("$wilsonが$sherlockの活躍を小説にすると言い出す"),
+            w.plot_resolve("$wilson提案により探偵社$sherlockとして看板を掲げて難事件解決を行うことになった"),
+            w.plot_note("$wilsonは偽物が語った通り、王室からの依頼を受けて活動しているエージェントだが、他にもライターをやっていた"),
+            w.plot_note("以前から新聞社の知人に小説を書かないかと頼まれていたらしい"),
+            w.plot_note("$maryたちは$sherlockにどうせなら事件解決を仕事にすればいいと提案される"),
+            w.plot_note("$wilsonは「探偵」という職業が別の国にはあると説明する"),
+            w.plot_note("こうして正式に事件解決会社「探偵社$sherlock」として看板を掲げ、商売を始めることになった"),
+            outline=OUTLINES[0])
 
 
 def wilsons_papers(w: World):
     return w.episode("$wilsonの手記",
-            w.plot_setup("本物の$wilsonが作品の記述者だった"),
-            w.plot_setup("$sherlockの活躍を小説として書き上げたのだ"),
-            w.plot_develop("小説を書き上げ、$wilsonは仲間の待つリビングに向かう"),
-            w.plot_develop("リビングでは今日も$maryがうるさくしていた"),
-            w.plot_develop("市場で花屋の手伝いをするようになった$maryが、もらったというお菓子を出してお茶会"),
-            w.plot_develop("$limeは王室には戻らず、今は剣の道場を手伝いつつ、守衛のバイトをしていた"),
-            w.plot_resolve("みんなそれぞれの仕事をし、新しい日々を送っている"),
-            w.plot_resolve("$sherlockは$heroなことを隠して今日も新聞記事を眺めては謎を探していた"),
-            w.plot_turnpoint("そこに依頼者がやってくる"),
-            )
+            # NOTE
+            w.plot_setup("作品の記述者は本物の方の$wilsonだった"),
+            w.plot_note("※$wilsonの一人称になり、ネタバラシがある"),
+            w.plot_note("偽物の$wilsonが本当は何をしていたのかについては$sherlockとは別の見解を持っている"),
+            w.plot_note("$sherlockから話を聞いただけでは事件概要と彼らの視点しか分からなかったので、自分で情報を集めて補完した"),
+            w.plot_note("小説として書いた以外にももちろん些細な情報を得ているが、その全てを記すには紙面が足りない"),
+            w.plot_note("一点だけ書き加えておくと、偽物の$wilsonは$boss復活のために遣わされた訳ではなく、わずかに残っていた資料（数冊の本が増えていた）から、自分で考えて$boss復活が最良の手段だと選んで実行したこと"),
+            w.plot_note("謎が多い$zeronについて、今後も調査をしてみることにする"),
+            w.plot_turnpoint("$maryが$wilsonを呼びに来る"),
+            w.plot_develop("一旦今回の事件の分を書き終えた$wilson"),
+            w.plot_note("部屋は余っていたが、二階のそれぞれの部屋を$mary、$lime、$sherlockに当てた"),
+            w.plot_note("お陰で書斎兼仕事部屋は物置になっている"),
+            w.plot_note("最近料理を覚え始めた$mary"),
+            w.plot_note("きっちり測ると同じ味にできるというのでお菓子作りが楽しいらしい"),
+            w.plot_note("リビングでは焼き立てのケーキがあった"),
+            w.plot_note("甘い物は苦手な$sherlock"),
+            w.plot_note("$maryは彼に食べさせたくて仕方ない"),
+            w.plot_note("まず$wilsonが味見することになっていた"),
+            w.plot_turnpoint("$maryのケーキを食べた時に過去の記憶を思い出す$wilson"),
+            w.plot_resolve("かつて$jackとして活動していた頃があるが、いずれそれについても語ることがあるだろうと締める"),
+            w.plot_note(""),# TODO
+            w.plot_note("そこに新たな依頼者が訪れる"),
+            w.plot_note("これがまた新しい$sherlockの事件の始まりだった"),
+            outline=OUTLINES[-1])
 
 
 # Chapter
