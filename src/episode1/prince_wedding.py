3bee27ec822f563134880f36432e26bb05a499ab
nagisc007 <nagisc007@yahoo.co.jp>
2021-01-17 17:38:28 +0900

    [WIP]

--- src/episode1/prince_wedding.py
+++ src/episode1/prince_wedding.py
@@ -7,11 +7,11 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.append('storybuilder')
 from storybuilder.builder.world import World
-from config import TITLES
 
 
-# Scenes
-# NOTE
+# DEFINE
+TITLE = "王子の結婚"
+
 
 # NOTE: outlines
 ABSTRACT = """
@@ -29,14 +29,20 @@ $jackの表の顔がオペラ歌手$ailyだった。
 """
 
 
+# Scenes
+
+
 # Episode
 def main(w: World):
-    return w.episode("王子の結婚",
+    return w.episode(TITLE,
             # NOTE
             w.plot_setup("$ailyこと怪盗$jackは$sherlockに全て見抜かれていたことで本性を現す"),
             w.plot_turnpoint("$jackは最初から$royalswordが目的で王子に近づいたと告白する"),
             w.plot_develop("$sherlockは$jackが本物の$ailyの自殺を偽装するために、強盗を装ったこと等を推理で当てた。彼女はすんなりと$sherlockに$royalswordの返却を約束する"),
             w.plot_turnpoint("しかし後日届けられた$royalswordには肝心の$red_stoneが付いていなかった"),
             w.plot_resolve("$stoneのレプリカを作ってもらい、何とか無事に王子の結婚式は行われた"),
+            w.plot_note("$sherlockは$yilaに対して「$ailyですよね？」と言った"),
+            w.plot_note("$yilaは分からないふりをする"),
+            w.plot_note(""),# TODO
             outline=ABSTRACT)
 
