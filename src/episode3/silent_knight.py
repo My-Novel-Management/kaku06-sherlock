a4df4dbdbf257173d0196b0de2130bb3d20e4bcf
nagisc007 <nagisc007@yahoo.co.jp>
2021-01-19 17:36:35 +0900

    [WIP]

--- src/episode3/silent_knight.py
+++ src/episode3/silent_knight.py
@@ -12,8 +12,6 @@ from storybuilder.builder.world import World
 # DEFINE
 TITLE = "沈黙の騎士"
 
-# Scenes
-# NOTE
 
 # NOTE: outlines
 ABSTRACT = """
@@ -36,5 +34,25 @@ def main(w: World):
             w.plot_turnpoint("$maryは市場で困っている赤い鎧騎士を見つける"),
             w.plot_resolve("$maryが困っていた鎧騎士を拾って連れて帰ってくる"),
             w.plot_turnpoint("$maryが連れてきた鎧騎士は喋ることができなかった"),
+            w.plot_note("$maryが一緒に暮らすようになった"),
+            w.plot_note("彼女は起きてくると、リビングで新聞を広げている$sherlockを見て「おはよう」と挨拶する"),
+            w.plot_note("返事のない$sherlockにきれながらもキッチンに向かう"),
+            w.plot_note("顔を洗い、エプロンをする"),
+            w.plot_note("朝食の準備をする"),
+            w.plot_note("焦げた目玉焼きにベーコン、パンにミルク"),
+            w.plot_note("$sherlockは何も言わずにそれを食べる"),
+            w.plot_note("そこに$wilsonが訪れる。「おはよう。ずいぶんと香ばしい臭いだね」"),
+            w.plot_note("相変わらず料理は下手なままで$wilsonにからかわれると、$maryは「うちかて必死にやってんねん」とむくれる"),
+            w.plot_note("「食事なんてお腹が膨れればいいんだよ」とそっけない$sherlock"),
+            w.plot_note("$wilsonにも出してくれて、そのプレートを食べるがまずい顔"),
+            w.plot_note("$maryは怒りながらも自分でもまずいらしい"),
+            w.plot_note("$maryはそれでも食べて、後片付けにキッチンに引っ込む"),
+            w.plot_note("$wilsonは$sherlockに失踪事件について何か進捗があったか尋ねる"),
+            w.plot_note("改めて$wilsonの依頼内容を整理する$sherlock"),
+            w.plot_note("第二王女が失踪したのは一ヶ月前。既に亡くなっていてもおかしくない"),
+            w.plot_note("謎の失踪事件については、普通の失踪事件と見分けがつかないから判断しようがないが、記事にはいくつも見つかる"),
+            w.plot_note("記事に掲載されるものの発生はばらばらで、頻度もよく分からない"),
+            w.plot_note("それとは別に女性の猟奇殺人が二件、近く発生している。このうちの一人が半月前に失踪していた"),
+            w.plot_note(""),# TODO
             outline=ABSTRACT)
 
